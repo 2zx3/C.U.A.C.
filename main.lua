b1aaa2517ce46cf9790b25f46567ebf4d53cbbf9
Ee <briarrose209@gmail.com>
2024-11-06 20:45:38 +0800

    Update main.lua
    Added the camera system

--- main.lua
+++ main.lua
@@ -1,9 +1,23 @@
--- Virtual Gamepad --
+ -- Virtual Gamepad --
 local lovepad = require "lovepad"
 lovepad:setGamePad()
 -- Virtual Gamepad --
 
 function love.load()
+  -- Camera --
+  camera = require "camera"
+  cam = camera()
+ -- Camera -- 
+ 
+lovepad:new{
+text = "Pause",
+x = 280,
+y = 50,
+length, width, side = 10,
+lovepad:remove(3)
+ }
+ 
+ -- player characteristics --
  player = {}
  player.character = love.graphics.newImage("placeholder.jpg")
  Charawidth  = player.character:getWidth()
@@ -11,6 +25,8 @@ function love.load()
  player.x = 400
  player.y = 200
  CharaInteract = love.graphics.newImage("2024-03-30.jpg")
+ Map = love.graphics.newImage("placeholder(1).jpg")
+ -- player characteristics --
 end
 
 function love.update(dt)
@@ -36,11 +52,17 @@ if lovepad:isPressed('A') then
 end
    
    if lovepad:isPressed('B') then
-     player.x = player.x - 40
+ player.x = player.x - 40
    end
-  end
- 
-function love.draw()
+  
+    cam:lookAt(player.x, player.y)
+  
+   end
+  
+ function love.draw()
+  cam:attach()
+    love.graphics.draw(Map)
+    love.graphics.draw(player.character, player.x, player.y, 0, 1, 1,  Charawidth/2, Charaheight/2)
+  cam:detach()
   lovepad:draw()
-  love.graphics.draw(player.character, player.x, player.y, nil, 1, 1, Charawidth/2, Charaheight/2)
-end
+ end
