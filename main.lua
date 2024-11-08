31f115c3ed79f289d292f3c898a985aff566abe1
Ee <briarrose209@gmail.com>
2024-11-08 09:53:23 +0800

    Turned main.lua into a scene manager
    Why not

--- main.lua
+++ main.lua
@@ -1,3 +1,21 @@
+function love.load()
+  Scene = require("Scenes/Test")
+ if Scene.load then Scene:load()
+ end
+end
+
+function love.update(dt)
+  if Scene.update then Scene:update(dt)
+ end
+end
+
 function love.draw()
-	love.graphics.print("nigga")
+  if Scene.draw then Scene:draw()
+ end
+end
+
+function changeScene(nextScene)
+  Scene = require("Scenes/"..nextScene)
+  if Scene.load then Scene:load()
+ end
 end
