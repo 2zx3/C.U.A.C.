04b12f92a304c9a2fa871f8fbc3d232cb750d2a6
Ee <briarrose209@gmail.com>
2024-11-11 17:19:56 +0800

    Add files via upload

---
+++ Scenes/pazmenu.lua
@@ -0,0 +1,23 @@
+require "main"
+
+local pausemenu = {}
+
+dimsX, dimsY = lg.getDimensions()
+function pausemenu:load()
+  
+end
+
+function pausemenu:update(dt)
+  
+end
+
+function love.touchpressed(id, x, y, dx, dy, pressure)
+  changeScene("gametest")
+end
+
+function pausemenu:draw()
+  love.graphics.print("Game Paused", dimsX/3.25, dimsY/4, 0, 3, 3)
+  love.graphics.print("Press Anywhere to Continue... again.", dimsX/3.25, dimsY-15, 0, 1, 1)
+end
+
+return pausemenu
